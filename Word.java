05a4d11d64b17b4d3002d7e8c88490f9bf976281
amitatiwari255 <amita.tiwari255@gmail.com>
2022-11-28 18:49:30 +0530

    Added to print  Word

---
+++ Word.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Word {
+	 public static void main(String[] args) {
+	        Scanner s = new Scanner(System.in);
+	        System.out.println("Enter a single digit to print in word");
+	        int i = s.nextInt();
+	        String [] str = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+	        if (i<10 && i>=0){
+	            System.out.println(str[i]);
+	        }else
+	            System.out.println("Wrong input");
+	    }
+
+}
